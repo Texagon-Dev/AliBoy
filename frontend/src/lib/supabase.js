e3c8c0d0605940e0f689eda71814d53d3be6846f
Muhammad Talha <talhazulf4163@gmail.com>
2024-04-24 15:01:23 +0500

    updates

--- frontend/src/lib/supabase.js
+++ frontend/src/lib/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-console.log(import.meta.env.VITE_SUPABASE_URL);
-
 const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON)
 
 export default supabase
