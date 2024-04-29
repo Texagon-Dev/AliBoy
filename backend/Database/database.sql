350334b0c689d7fe6a914f68c8e2f79ac975c01b
klaus073 <nomirao1254@gmail.com>
2024-04-29 18:10:06 +0500

    Created DB Schema For Auth and Story Books and Book Printing Records

--- backend/Database/database.sql
+++ backend/Database/database.sql
@@ -1,35 +1,47 @@
-CREATE TABLE "Users" (
-    user_id VARCHAR PRIMARY KEY,
-    users_email VARCHAR NOT NULL,
-    users_password VARCHAR NOT NULL,
-    last_login TIMESTAMP
-   
-);
+create table
+  public.users (
+    created_at timestamp with time zone not null default now(),
+    uuid uuid not null,
+    email character varying not null,
+    metadata jsonb null default '[]'::jsonb,
+    constraint customer_pkey primary key (uuid),
+    constraint customer_uuid_fkey foreign key (uuid) references auth.users (id) on delete cascade
+  ) tablespace pg_default;
 
-CREATE TABLE "Users_Profile" (
-    user_id VARCHAR PRIMARY KEY,
-    full_name VARCHAR NOT NULL,
-    dob TIMESTAMP NOT NULL,
-    profile_picture VARCHAR DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    
-);
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = public
+as $$
+begin
+  insert into public."customer" ("uuid", "email", "metadata")
+  values (new.id, new.email, new.raw_user_meta_data);
+  return new;
+end;
+$$;
+
+create or replace trigger on_auth_user_created
+after insert on auth.users
+for each row
+execute procedure public.handle_new_user();
 
 CREATE TABLE "User_Story_Books" (
     story_book_id SERIAL PRIMARY KEY,
-    user_id VARCHAR NOT NULL,
+    uuid UUID NOT NULL,
     story_name VARCHAR NOT NULL,
     pdf_path VARCHAR DEFAULT NULL,
     tags VARCHAR NOT NULL,
     story_picture VARCHAR DEFAULT NULL,
-    created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "Users"(user_id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (uuid) REFERENCES "users"(uuid)
 );
+
 
 CREATE TABLE "Book_Printing_Orders" (
     printing_id SERIAL PRIMARY KEY,
     story_book_id INTEGER NOT NULL,
-    user_id VARCHAR NOT NULL,
+    uuid UUID NOT NULL,
     binding_name VARCHAR NOT NULL,
     title_size VARCHAR NOT NULL,
     quantity INTEGER NOT NULL,
@@ -41,7 +53,7 @@ CREATE TABLE "Book_Printing_Orders" (
     discount FLOAT NOT NULL,
     shipping_amount FLOAT NOT NULL,
     payment_method VARCHAR NOT NULL,
-    order_status VARCHAR DEFAULT 'Pending',
-    FOREIGN KEY (user_id) REFERENCES "Users"(user_id),
+    order_status VARCHAR DEFAULT 'Pending', -- Default value set to 'Pending'
+    FOREIGN KEY (uuid) REFERENCES "users"(uuid),
     FOREIGN KEY (story_book_id) REFERENCES "User_Story_Books"(story_book_id)
 );
