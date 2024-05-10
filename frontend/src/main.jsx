7985eb68ced214e7e0393faf352d2fc47f0c285e
aqibaliakbar <aqibaliakbar@gmail.com>
2024-05-10 16:19:59 +0500

    Redux Persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux';
-import store from './redux/store.jsx';
 
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
- 
+    </PersistGate>
+  </Provider>
 );
--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -1,16 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import bookPrintingOrderReducer from "./features/bookPrintingSlice";
 import customerOrdersReducer from "./features/customerOrdersSlice";
 import userReducer from "./features/userSlice";
 import storiesReducer from "./features/storySlice";
+import userStoriesReducer from "./features/userStoriesSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"], // Only user state will be persisted
+};
+
+const rootReducer = {
+  bookPrintingOrders: bookPrintingOrderReducer,
+  customerOrders: customerOrdersReducer,
+  user: persistReducer(persistConfig, userReducer), // Wrap user reducer
+  userStories: userStoriesReducer,
+  stories: storiesReducer,
+};
 
 const store = configureStore({
-  reducer: {
-    bookPrintingOrders: bookPrintingOrderReducer,
-    customerOrders: customerOrdersReducer,
-    user: userReducer,
-    stories: storiesReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"], // ignore non-serializable checks for specific actions
+      },
+    }),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };
